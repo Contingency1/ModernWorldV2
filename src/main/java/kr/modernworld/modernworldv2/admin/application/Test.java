1c6e6d7e57047ef2f8e294012c3e868dc37e53eb
JYE <jo1814@naver.com>
2025-09-25 16:16:06 +0900

    🧹chore: 패키지 구조 세팅용 Test Class 추가 (#9)

---
+++ src/main/java/kr/modernworld/modernworldv2/admin/application/Test.java
@@ -0,0 +1,5 @@
+package kr.modernworld.modernworldv2.admin.application;
+
+public class Test {
+
+}
---
+++ src/main/java/kr/modernworld/modernworldv2/admin/infrastructure/Test.java
@@ -0,0 +1,5 @@
+package kr.modernworld.modernworldv2.admin.infrastructure;
+
+public class Test {
+
+}
