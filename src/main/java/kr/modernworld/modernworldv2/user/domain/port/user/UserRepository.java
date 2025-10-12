f2f171fae51cded8867256b90df7b36bd6b82a12
JYE <jo1814@naver.com>
2025-10-13 04:24:45 +0900

    ✨feat: user 정보를 저장할 userRepository 생성 (#11)

---
+++ src/main/java/kr/modernworld/modernworldv2/user/domain/port/user/UserRepository.java
@@ -0,0 +1,11 @@
+package kr.modernworld.modernworldv2.user.domain.port.user;
+
+import java.util.Optional;
+import kr.modernworld.modernworldv2.user.domain.user.User;
+
+public interface UserRepository {
+
+  Optional<User> findByUniqueIdentifier(String uniqueIdentifier);
+
+  User save(User user);
+}
