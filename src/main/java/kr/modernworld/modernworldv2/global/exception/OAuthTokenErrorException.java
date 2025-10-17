0e3379b44b461ff9c9a473390fe6e46829ddbe9c
JYE <jo1814@naver.com>
2025-10-17 19:42:51 +0900

    ♻️refactor: OAuth 관련 에러 class 이름 변경 (#11)

--- src/main/java/kr/modernworld/modernworldv2/global/exception/OAuthTokenErrorException.java
+++
@@ -1,8 +0,0 @@
-package kr.modernworld.modernworldv2.global.exception;
-
-public class OAuthTokenErrorException extends RuntimeException {
-
-  public OAuthTokenErrorException(String message) {
-    super(message);
-  }
-}
