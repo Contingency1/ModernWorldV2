537260df70e357b6f6409c248137babdbbb6f9bb
JYE <jo1814@naver.com>
2025-10-18 23:49:05 +0900

    ✨feat: 구글 관련 client 구현 및 가독성 향상 (#11)

--- src/main/java/kr/modernworld/modernworldv2/user/infrastructure/auth/google/GoogleOAuthClient.java
+++ src/main/java/kr/modernworld/modernworldv2/user/infrastructure/auth/google/GoogleOAuthClient.java
@@ -1,29 +1,40 @@
 package kr.modernworld.modernworldv2.user.infrastructure.auth.google;
 
+import kr.modernworld.modernworldv2.global.exception.OAuthException;
 import kr.modernworld.modernworldv2.user.application.oauth.OAuthTokenDTO;
 import kr.modernworld.modernworldv2.user.application.oauth.SocialUserInfoDTO;
 import kr.modernworld.modernworldv2.user.domain.port.OAuthClient;
 import kr.modernworld.modernworldv2.user.domain.user.UserDomain;
+import kr.modernworld.modernworldv2.user.infrastructure.auth.google.dto.GoogleApiErrorDTO;
+import kr.modernworld.modernworldv2.user.infrastructure.auth.google.dto.GoogleTokenFailDTO;
+import kr.modernworld.modernworldv2.user.infrastructure.auth.google.dto.GoogleTokenSuccessDTO;
+import kr.modernworld.modernworldv2.user.infrastructure.auth.google.dto.GoogleUserInfoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
 @Service
 public class GoogleOAuthClient implements OAuthClient {
 
-  private final WebClient webClient;
+  private final WebClient authWebClient;
+  private final WebClient apiWebClient;
   private final GoogleOAuthProperties properties;
-
-  private final String BASE_URL = "https://accounts.google.com";
 
   @Autowired
   public GoogleOAuthClient(WebClient webClient, GoogleOAuthProperties properties) {
-    this.webClient = webClient.mutate()
-        .baseUrl(BASE_URL)
-        .defaultHeader(HttpHeaders.CONTENT_TYPE,
-            MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    this.authWebClient = webClient.mutate()
+        .baseUrl("https://oauth2.googleapis.com")
+        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+        .build();
+
+    this.apiWebClient = webClient.mutate()
+        .baseUrl("https://www.googleapis.com")
+        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
         .build();
 
     this.properties = properties;
@@ -37,27 +48,79 @@ public class GoogleOAuthClient implements OAuthClient {
   @Override
   public String getLoginUrl(String state) {
     String path = "/o/oauth2/v2/auth";
-    String clientId = "?client_id=" + properties.id();
-    String redirectUri = "&redirect_uri=" + properties.callbackUrl();
-    String responseType = "&response_type=code";
-    String scope = "&scope=https://www.googleapis.com/auth/userinfo.profile";
-    String accessType = "&access_type=offline";
-    String finalState = "&state=" + state;
-    String prompt = "&prompt=consent";
 
-    return BASE_URL + path + clientId + redirectUri
-        + responseType + scope + accessType + finalState + prompt;
+    return UriComponentsBuilder
+        .fromUriString("https://accounts.google.com")
+        .path(path)
+        .queryParam("client_id", properties.id())
+        .queryParam("redirect_uri", properties.callbackUrl())
+        .queryParam("response_type", "code")
+        .queryParam("scope", "https://www.googleapis.com/auth/userinfo.profile")
+        .queryParam("access_type", "offline")
+        .queryParam("state", state)
+        .queryParam("prompt", "consent").encode().toUriString();
   }
-
 
   @Override
   public OAuthTokenDTO getSocialToken(String state, String authCode) {
-    return null;
+    GoogleTokenSuccessDTO response = getGoogleToken(authCode);
+
+    return new OAuthTokenDTO(response.accessToken(), response.refreshToken(), response.expiresIn(),
+        response.refreshTokenExpiresIn().orElse(response.expiresIn()));
+  }
+
+  private GoogleTokenSuccessDTO getGoogleToken(String authCode) {
+    return authWebClient
+        .post()
+        .uri("/token")
+        .body(BodyInserters.fromFormData("code", authCode)
+            .with("client_id", properties.id())
+            .with("client_secret", properties.secret())
+            .with("redirect_uri", properties.callbackUrl())
+            .with("grant_type", "authorization_code")
+        )
+        .exchangeToMono(res -> {
+          if (res.statusCode().isError()) {
+            return res.bodyToMono(GoogleTokenFailDTO.class).flatMap(error ->
+                Mono.error(new OAuthException(
+                    "[GoogleOAuthClient] OAuth token error: " + error.error().orElse(null)
+                        + ", Description: " + error.errorDescription().orElse(null))));
+          }
+          return res.bodyToMono(GoogleTokenSuccessDTO.class);
+        })
+        .switchIfEmpty(Mono.error(
+            new IllegalStateException("[GoogleOAuthClient] Google token response body is empty.")))
+        .block();
   }
 
   @Override
   public SocialUserInfoDTO getSocialUserInfo(String socialAccessToken) {
-    return null;
+    String uri = "/oauth2/v2/userinfo";
+
+    GoogleUserInfoDTO response = getGoogleUserInfo(socialAccessToken, uri);
+
+    return new SocialUserInfoDTO(response.id(), response.name(), response.picture());
+  }
+
+  private GoogleUserInfoDTO getGoogleUserInfo(String socialAccessToken, String uri) {
+    return apiWebClient
+        .get()
+        .uri(uri)
+        .header(HttpHeaders.AUTHORIZATION, "Bearer " + socialAccessToken)
+        .exchangeToMono(res -> {
+          if (res.statusCode().isError()) {
+            return res.bodyToMono(GoogleApiErrorDTO.class)
+                .flatMap(error -> Mono.error(new OAuthException(
+                    "[GoogleOAuthClient] OAuth token error: " + error.error().code().orElse(null)
+                        + ", Description: " + error.error().message().orElse(null)
+                        + ", status: " + error.error().status().orElse(null))));
+          }
+
+          return res.bodyToMono(GoogleUserInfoDTO.class);
+        })
+        .switchIfEmpty(Mono.error(
+            new IllegalStateException("[GoogleOAuthClient] Google API response body is empty.")))
+        .block();
   }
 
 }
