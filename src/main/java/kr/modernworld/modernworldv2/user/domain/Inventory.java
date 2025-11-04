cbf5376b25b63302b0847158b6ec08f0d259b094
JYE <jo1814@naver.com>
2025-11-04 16:39:27 +0900

    ✨feat: Inventory 관련 도메인 객체 생성 (#14)
    - 기본적으로 AR인 InventoryCollection은 Inventory의 생명주기를 관리함.
    - 그러나 AR을 통하지 않고도 Inventory 객체 자체가 생성 될 수 있음.

---
+++ src/main/java/kr/modernworld/modernworldv2/user/domain/Inventory.java
@@ -0,0 +1,47 @@
+package kr.modernworld.modernworldv2.user.domain;
+
+import java.time.Instant;
+import kr.modernworld.modernworldv2.admin.domain.ItemType;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class Inventory {
+
+  private final Long no;
+  private final Long userNo;
+  private final Long itemNo;
+  private Boolean status;
+  private final ItemType itemType;
+  private final Instant createdAt;
+
+  @Builder
+  private Inventory(Long no, Long userNo, Long itemNo, Boolean status, ItemType itemType,
+      Instant createdAt) {
+    this.no = no;
+    this.userNo = userNo;
+    this.itemNo = itemNo;
+    this.status = status;
+    this.itemType = itemType;
+    this.createdAt = createdAt;
+  }
+
+  public static Inventory create(Long userNo, Long itemNo, ItemType itemType) {
+    return Inventory.builder()
+        .userNo(userNo)
+        .itemNo(itemNo)
+        .itemType(itemType)
+        .status(false)
+        .createdAt(Instant.now())
+        .build();
+  }
+
+  public void makeStatusTrue() {
+    this.status = true;
+  }
+
+  public void makeStatusFalse() {
+    this.status = false;
+  }
+
+}
