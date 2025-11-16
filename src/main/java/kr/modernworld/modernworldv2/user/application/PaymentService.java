c4c76a6277a88ae4662fa739aff61a46faca2d88
JYE <jo1814@naver.com>
2025-11-16 17:04:48 +0900

    ✨feat: PaymentService 구현 (#17)
    - 유저가 포인트를 소비할 때 쓸 메서드 구현

--- src/main/java/kr/modernworld/modernworldv2/user/application/PaymentService.java
+++ src/main/java/kr/modernworld/modernworldv2/user/application/PaymentService.java
@@ -1,10 +1,7 @@
 package kr.modernworld.modernworldv2.user.application;
 
-import kr.modernworld.modernworldv2.admin.application.api.ItemApi;
-import kr.modernworld.modernworldv2.admin.application.api.ItemPriceAndTypeDTO;
 import kr.modernworld.modernworldv2.global.error.BusinessErrorCode;
 import kr.modernworld.modernworldv2.global.error.BusinessException;
-import kr.modernworld.modernworldv2.user.domain.Inventory;
 import kr.modernworld.modernworldv2.user.domain.port.user.UserQueryRepository;
 import kr.modernworld.modernworldv2.user.domain.port.user.UserRepository;
 import kr.modernworld.modernworldv2.user.domain.user.User;
@@ -14,31 +11,24 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class ItemShopService {
+public class PaymentService {
 
-  private final ItemApi itemApi;
   private final UserQueryRepository userQueryRepository;
-  private final InventoryService inventoryService;
   private final UserRepository userRepository;
 
   @Transactional
-  public Inventory buyOneItem(Long userNo, Long itemNo) {
-    inventoryService.validateItemNotExists(userNo, itemNo);
-
+  public void pay(Long userNo, Long price) {
     User user = userQueryRepository.findUserCurrentPointByNo(userNo)
         .orElseThrow(() -> new BusinessException(BusinessErrorCode.USER_NOT_FOUND));
 
-    ItemPriceAndTypeDTO itemInfo = itemApi.getPrice(itemNo);
-
     try {
-      user.decreaseCurrentPoint(itemInfo.price());
+      user.decreaseCurrentPoint(price);
     } catch (IllegalArgumentException e) {
       throw new BusinessException(BusinessErrorCode.USER_NOT_HAS_ENOUGH_POINT,
           " " + e.getMessage());
     }
 
     userRepository.updateCurrentPoint(user.getNo(), user.getCurrentPoint());
-    return inventoryService.addOneItemInInventory(userNo, itemNo, itemInfo.type());
   }
 
 }
