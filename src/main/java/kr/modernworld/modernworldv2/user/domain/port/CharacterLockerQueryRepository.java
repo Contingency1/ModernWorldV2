d0522cef9fa71ce5f55bff6795aefaac2217878e
JYE <jo1814@naver.com>
2025-11-16 17:06:03 +0900

    ✨feat: CharacterLockerQueryRepository, CharacterLockerRepository 구현 (#17)
    - 구현체, 인터페이스 생성

--- src/main/java/kr/modernworld/modernworldv2/user/domain/port/CharacterLockerQueryRepository.java
+++ src/main/java/kr/modernworld/modernworldv2/user/domain/port/CharacterLockerQueryRepository.java
@@ -2,12 +2,15 @@ package kr.modernworld.modernworldv2.user.domain.port;
 
 import java.util.List;
 import kr.modernworld.modernworldv2.admin.domain.CharacterSpecies;
-import kr.modernworld.modernworldv2.user.presentation.dto.res.GetCharacterLockerResponseDTO;
+import kr.modernworld.modernworldv2.user.domain.characterlocker.CharacterLockerCollection;
+import kr.modernworld.modernworldv2.user.presentation.characterlocker.dto.res.GetCharacterLockerResponseDTO;
 
 public interface CharacterLockerQueryRepository {
 
-  List<GetCharacterLockerResponseDTO> getUserCharacters(Long userNo, Boolean status,
+  List<GetCharacterLockerResponseDTO> getUserCharactersByCondition(Long userNo, Boolean status,
       CharacterSpecies species);
 
-  Boolean exists(Long userNo, CharacterSpecies species);
+  CharacterLockerCollection getUserAllCharacters(Long userNo);
+
+  Boolean exists(Long userNo, Long characterNo);
 }
