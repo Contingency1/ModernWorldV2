504679674b7e22a775db404dd3c828e61f80f498
JYE <jo1814@naver.com>
2025-11-16 17:36:37 +0900

    🛠️fix: CharacterLockerJPARepository 잘못된 제네릭 설정 변경 (#17)

--- src/main/java/kr/modernworld/modernworldv2/user/infrastructure/repository/CharacterLockerJPARepository.java
+++ src/main/java/kr/modernworld/modernworldv2/user/infrastructure/repository/CharacterLockerJPARepository.java
@@ -1,8 +1,11 @@
 package kr.modernworld.modernworldv2.user.infrastructure.repository;
 
-import kr.modernworld.modernworldv2.admin.infrastructure.persistence.entity.CharacterJPAEntity;
+import kr.modernworld.modernworldv2.user.infrastructure.persistence.entity.CharacterLockerJPAEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CharacterLockerJPARepository extends JpaRepository<CharacterJPAEntity, Long> {
+@Repository
+public interface CharacterLockerJPARepository extends
+    JpaRepository<CharacterLockerJPAEntity, Long> {
 
 }
