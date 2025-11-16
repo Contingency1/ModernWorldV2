e62c956e2a9b710d402407bbfc7217b3b9cd13f8
JYE <jo1814@naver.com>
2025-11-16 17:01:15 +0900

    ✨feat: character Query Repository interface 생성및 구현체 구현 (#17)
    - 가격 조회 메서드 추가

---
+++ src/main/java/kr/modernworld/modernworldv2/admin/infrastructure/repository/character/CharacterQueryRepositoryImpl.java
@@ -0,0 +1,32 @@
+package kr.modernworld.modernworldv2.admin.infrastructure.repository.character;
+
+
+import static kr.modernworld.modernworldv2.admin.infrastructure.persistence.entity.QCharacterJPAEntity.characterJPAEntity;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
+import kr.modernworld.modernworldv2.admin.domain.port.character.CharacterQueryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CharacterQueryRepositoryImpl implements CharacterQueryRepository {
+
+  private final JPAQueryFactory jpaQuery;
+
+  @Override
+  public Optional<Long> getPrice(Long no) {
+    Long price = jpaQuery
+        .select(characterJPAEntity.price)
+        .from(characterJPAEntity)
+        .where(characterJPAEntity.no.eq(no))
+        .fetchOne();
+
+    if (price == null) {
+      return Optional.empty();
+    }
+    
+    return Optional.of(price);
+  }
+}
