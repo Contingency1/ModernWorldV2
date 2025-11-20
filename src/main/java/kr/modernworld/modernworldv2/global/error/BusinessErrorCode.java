d3061a9d95db670c2da1d5e32c73ce98a11f32ab
JYE <jo1814@naver.com>
2025-11-20 16:26:57 +0900

    ✨feat: BusinessErrorCode Exception 추가 (#16)

--- src/main/java/kr/modernworld/modernworldv2/global/error/BusinessErrorCode.java
+++ src/main/java/kr/modernworld/modernworldv2/global/error/BusinessErrorCode.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum BusinessErrorCode {
-  NO_SUCH_ITEM(HttpStatus.NOT_FOUND, "There is no item."),
+  ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "There is no item."),
 
   NO_SUCH_CHARACTER(HttpStatus.NOT_FOUND, "There is no character."),
 
@@ -18,7 +18,11 @@ public enum BusinessErrorCode {
   ITEM_NOT_FOUND_IN_INVENTORY(HttpStatus.NOT_FOUND, "User doesn't have that item."),
 
   USER_NOT_HAS_ENOUGH_POINT(HttpStatus.FORBIDDEN, "User does not have enough point."),
-  USER_NOT_FOUND(HttpStatus.FORBIDDEN, "User not found.");
+  USER_NOT_FOUND(HttpStatus.FORBIDDEN, "User not found."),
+
+  PRESENT_INVALID_STATE(HttpStatus.CONFLICT, "Cannot process the present request."),
+  PRESENT_NOT_FOUND(HttpStatus.NOT_FOUND, "Present not found."),
+  PRESENT_ACCESS_DENIED(HttpStatus.FORBIDDEN, "Present access denied.");
 
   BusinessErrorCode(HttpStatus status, String message) {
     this.status = status;
