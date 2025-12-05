8f558e6762f541d07764ffe5d50d102593522426
JYE <jo1814@naver.com>
2025-12-05 20:16:09 +0900

    ✨feat: Legend 관련 로직 추가, 수정 (#25)
    - Legend 도메인 객체 LegendField를 이용한 업적 기록 증가 메서드 최적화
    - LegendRepository save 메서드 Legend 반환 수정
    - LegendRepository findByUserNoForUpdate 메서드 추가 -> Lock Mode PESSIMISTIC_WRITE 설정
    - LegendService update 메서드 생성

--- src/main/java/kr/modernworld/modernworldv2/user/infrastructure/repository/legend/LegendRepositoryImpl.java
+++ src/main/java/kr/modernworld/modernworldv2/user/infrastructure/repository/legend/LegendRepositoryImpl.java
@@ -1,6 +1,11 @@
 package kr.modernworld.modernworldv2.user.infrastructure.repository.legend;
 
+import static kr.modernworld.modernworldv2.user.infrastructure.persistence.entity.QLegendJPAEntity.legendJPAEntity;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.LockModeType;
+import java.util.Optional;
 import kr.modernworld.modernworldv2.user.domain.Legend;
 import kr.modernworld.modernworldv2.user.domain.port.legend.LegendRepository;
 import kr.modernworld.modernworldv2.user.infrastructure.mapper.LegendMapper;
@@ -16,15 +21,33 @@ public class LegendRepositoryImpl implements LegendRepository {
   private final LegendJPARepository legendJPARepository;
   private final LegendMapper legendMapper;
   private final EntityManager entityManager;
+  private final JPAQueryFactory queryFactory;
 
   @Override
-  public void save(Legend legend) {
+  public Legend save(Legend legend) {
     LegendJPAEntity entity = legendMapper.toEntity(legend);
 
     if (legend.getUserNo() != null) {
       entity.setUser(entityManager.getReference(UserJPAEntity.class, legend.getUserNo()));
     }
 
-    legendJPARepository.save(entity);
+    return legendMapper.toDomain(legendJPARepository.save(entity));
+  }
+
+  @Override
+  public Optional<Legend> findByUserNoForUpdate(Long userNo) {
+
+    LegendJPAEntity entity = queryFactory
+        .select(legendJPAEntity)
+        .from(legendJPAEntity)
+        .where(legendJPAEntity.user.no.eq(userNo))
+        .setLockMode(LockModeType.PESSIMISTIC_WRITE)
+        .fetchOne();
+
+    if (entity == null) {
+      return Optional.empty();
+    }
+
+    return Optional.of(legendMapper.toDomain(entity));
   }
 }
