3e99b05ed6cce2ac2560e86fd8357822efa06a0e
JYE <jo1814@naver.com>
2025-12-05 20:01:24 +0900

    🛠️fix: Item 관련 가격만 query 하던 로직 수정 (#25)
    - item name과 Price를 가져오도록 수정

---
+++ src/main/java/kr/modernworld/modernworldv2/admin/application/api/ItemNameAndPriceDTO.java
@@ -0,0 +1,8 @@
+package kr.modernworld.modernworldv2.admin.application.api;
+
+public record ItemNameAndPriceDTO(
+    String name,
+    Long price
+) {
+
+}
--- src/main/java/kr/modernworld/modernworldv2/admin/infrastructure/repository/item/ItemQueryRepositoryImpl.java
+++ src/main/java/kr/modernworld/modernworldv2/admin/infrastructure/repository/item/ItemQueryRepositoryImpl.java
@@ -2,8 +2,10 @@ package kr.modernworld.modernworldv2.admin.infrastructure.repository.item;
 
 import static kr.modernworld.modernworldv2.admin.infrastructure.persistence.entity.QItemJPAEntity.itemJPAEntity;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.Optional;
+import kr.modernworld.modernworldv2.admin.application.api.ItemNameAndPriceDTO;
 import kr.modernworld.modernworldv2.admin.domain.port.item.ItemQueryRepository;
 import kr.modernworld.modernworldv2.admin.infrastructure.persistence.entity.ItemJPAEntity;
 import lombok.RequiredArgsConstructor;
@@ -16,18 +18,19 @@ public class ItemQueryRepositoryImpl implements ItemQueryRepository {
   private final JPAQueryFactory queryFactory;
 
   @Override
-  public Optional<Long> getPrice(Long itemNo) {
-    Long price = queryFactory
-        .select(itemJPAEntity.price)
+  public Optional<ItemNameAndPriceDTO> findNameAndPrice(Long itemNo) {
+    ItemNameAndPriceDTO nameAndPrice = queryFactory
+        .select(Projections.constructor(ItemNameAndPriceDTO.class,
+            itemJPAEntity.name, itemJPAEntity.price))
         .from(itemJPAEntity)
         .where(itemJPAEntity.no.eq(itemNo))
         .fetchFirst();
 
-    if (price == null) {
+    if (nameAndPrice == null) {
       return Optional.empty();
     }
 
-    return Optional.of(price);
+    return Optional.of(nameAndPrice);
   }
 
   @Override
