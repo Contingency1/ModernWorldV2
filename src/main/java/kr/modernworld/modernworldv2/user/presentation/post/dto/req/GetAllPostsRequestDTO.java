80dc788ee89d367d3c50cf5bcd41723ec391dcb6
JYE <jo1814@naver.com>
2025-12-07 15:41:37 +0900

    ✨feat: Post Controller 생성, 관련 DTO 생성 (#23)

---
+++ src/main/java/kr/modernworld/modernworldv2/user/presentation/post/dto/req/GetAllPostsRequestDTO.java
@@ -0,0 +1,12 @@
+package kr.modernworld.modernworldv2.user.presentation.post.dto.req;
+
+import kr.modernworld.modernworldv2.global.common.OrderBy;
+import kr.modernworld.modernworldv2.global.common.SenderReceiverNoField;
+
+public record GetAllPostsRequestDTO(
+    SenderReceiverNoField senderReceiverNoField,
+    OrderBy orderBy
+) {
+
+
+}
---
+++ src/main/java/kr/modernworld/modernworldv2/user/presentation/post/dto/res/PostUserInfoDTO.java
@@ -0,0 +1,8 @@
+package kr.modernworld.modernworldv2.user.presentation.post.dto.res;
+
+public record PostUserInfoDTO(
+    Long no,
+    String nickname
+) {
+
+}
