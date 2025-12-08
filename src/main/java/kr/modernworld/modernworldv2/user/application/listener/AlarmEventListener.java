89303108abe948761736db0d344ed558e6c49c99
JYE <jo1814@naver.com>
2025-12-08 15:41:50 +0900

    🛠️fix: AlarmEventListener 잘못 설정된 어노테이션 변경 (#25)
    - EventListener 와 TransactionalEventListener를 동시 사용해서 해당 트랜잭션이 실행되지 않았던 버그 수정
    - 새로운 트랜잭션에서 알람을 저장 /sse 전송 가능하도록 구현

--- src/main/java/kr/modernworld/modernworldv2/user/application/listener/AlarmEventListener.java
+++ src/main/java/kr/modernworld/modernworldv2/user/application/listener/AlarmEventListener.java
@@ -6,8 +6,9 @@ import kr.modernworld.modernworldv2.user.application.sse.SseEmitterService;
 import kr.modernworld.modernworldv2.user.application.sse.SseEvent;
 import kr.modernworld.modernworldv2.user.domain.alarm.AlarmTitle;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -18,8 +19,8 @@ public class AlarmEventListener {
   private final AlarmService alarmService;
   private final SseEmitterService sseEmitterService;
 
-  @EventListener
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
   public void onAlarmEvent(AlarmEvent event) {
     Long userNo = event.getUserNo();
     AlarmTitle title = event.getTitle();
