b48f43446e30c455d0a0c28ea05fb6ac9b9ca9e5
JYE <jo1814@naver.com>
2025-12-14 19:49:46 +0900

    ✨feat: neighbor 관련 CRUD 추가 (#21)

---
+++ src/main/java/kr/modernworld/modernworldv2/user/infrastructure/repository/neighbor/NeighborRepositoryImpl.java
@@ -0,0 +1,76 @@
+package kr.modernworld.modernworldv2.user.infrastructure.repository.neighbor;
+
+import static kr.modernworld.modernworldv2.user.infrastructure.persistence.entity.QNeighborJPAEntity.neighborJPAEntity;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.LockModeType;
+import java.util.Optional;
+import kr.modernworld.modernworldv2.user.domain.neighbor.Neighbor;
+import kr.modernworld.modernworldv2.user.domain.neighbor.port.NeighborRepository;
+import kr.modernworld.modernworldv2.user.infrastructure.mapper.NeighborMapper;
+import kr.modernworld.modernworldv2.user.infrastructure.persistence.entity.NeighborJPAEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class NeighborRepositoryImpl implements NeighborRepository {
+
+  private final NeighborJPARepository neighborJPARepository;
+  private final JPAQueryFactory queryFactory;
+  private final NeighborMapper neighborMapper;
+
+  @Override
+  public Neighbor save(Neighbor neighbor) {
+    NeighborJPAEntity entity = neighborJPARepository.save(neighborMapper.toEntity(neighbor));
+
+    return neighborMapper.toDomain(entity);
+  }
+
+  @Override
+  public Long delete(Long neighborNo, Long userNo) {
+    return queryFactory
+        .delete(neighborJPAEntity)
+        .where(neighborJPAEntity.no.eq(neighborNo),
+            neighborJPAEntity.sender.no.eq(userNo)
+                .or(neighborJPAEntity.receiver.no.eq(neighborNo)))
+        .execute();
+  }
+
+  @Override
+  public Optional<Neighbor> findOneThatStatusIsFalseForUpdate(Long senderNo,
+      Long receiverNo) {
+    NeighborJPAEntity entity = queryFactory
+        .selectFrom(neighborJPAEntity)
+        .where(
+            neighborJPAEntity.sender.no.eq(receiverNo),
+            neighborJPAEntity.receiver.no.eq(senderNo),
+            neighborJPAEntity.status.eq(false))
+        .setLockMode(LockModeType.PESSIMISTIC_WRITE)
+        .fetchFirst();
+
+    if (entity == null) {
+      return Optional.empty();
+    }
+
+    return Optional.of(neighborMapper.toDomain(entity));
+  }
+
+  @Override
+  public Optional<Neighbor> findByNoAndReceiverNoForUpdate(Long neighborNo, Long receiverNo) {
+    NeighborJPAEntity entity = queryFactory
+        .selectFrom(neighborJPAEntity)
+        .where(
+            neighborJPAEntity.no.eq(neighborNo),
+            neighborJPAEntity.receiver.no.eq(receiverNo))
+        .setLockMode(LockModeType.PESSIMISTIC_WRITE)
+        .fetchFirst();
+
+    if (entity == null) {
+      return Optional.empty();
+    }
+
+    return Optional.of(neighborMapper.toDomain(entity));
+  }
+
+}
