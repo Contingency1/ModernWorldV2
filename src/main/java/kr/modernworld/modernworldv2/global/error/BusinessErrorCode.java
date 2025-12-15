795abb14441bea3b8f28b701c98b11e705d75451
JYE <jo1814@naver.com>
2025-12-15 17:45:35 +0900

    ✨feat: BusinessErrorCode 방명록 관련 예외 추가 (#24)

--- src/main/java/kr/modernworld/modernworldv2/global/error/BusinessErrorCode.java
+++ src/main/java/kr/modernworld/modernworldv2/global/error/BusinessErrorCode.java
@@ -9,6 +9,9 @@ public enum BusinessErrorCode {
   NO_SUCH_ALARM(HttpStatus.NOT_FOUND, "No such port"),
   ALARM_NOT_FOUND(HttpStatus.NOT_FOUND, "User doesn't have that port."),
 
+  COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "There is no comment."),
+  COMMENT_NOT_A_SENDER(HttpStatus.FORBIDDEN, "User is not a sender."),
+
   POST_NOT_FOUND(HttpStatus.NOT_FOUND, "There is no port."),
   POST_USER_NOT_HAVE(HttpStatus.FORBIDDEN, "This port is not port's"),
   POST_CANNOT_POST_TO_YOURSELF(HttpStatus.FORBIDDEN,
