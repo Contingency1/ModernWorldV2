8abdf48d22a2eb73246e538bb229c5fc1cddd54f
JYE <jo1814@naver.com>
2025-12-15 17:48:24 +0900

    ✨feat: Comment 관련 필요한 타입 생성 (#24)

---
+++ src/main/java/kr/modernworld/modernworldv2/user/presentation/comment/dto/req/CreateCommentRequestDTO.java
@@ -0,0 +1,13 @@
+package kr.modernworld.modernworldv2.user.presentation.comment.dto.req;
+
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record CreateCommentRequestDTO(
+    @Size(min = 1, max = 100)
+    @NotNull
+    String content
+) {
+
+}
