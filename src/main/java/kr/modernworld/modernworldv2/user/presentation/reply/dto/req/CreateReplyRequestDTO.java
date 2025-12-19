4a3e4067f07b70c3bc9626fb5ef51e890f9a8872
JYE <jo1814@naver.com>
2025-12-19 15:38:28 +0900

    ✨feat: CreateReplyRequestDTO validation 추가 (#38)
    - content의 값은 null이 되면 안됨.

--- src/main/java/kr/modernworld/modernworldv2/user/presentation/reply/dto/req/CreateReplyRequestDTO.java
+++ src/main/java/kr/modernworld/modernworldv2/user/presentation/reply/dto/req/CreateReplyRequestDTO.java
@@ -1,8 +1,10 @@
 package kr.modernworld.modernworldv2.user.presentation.reply.dto.req;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CreateReplyRequestDTO(
+    @NotNull
     @Size(min = 1, max = 100)
     String content
 ) {
