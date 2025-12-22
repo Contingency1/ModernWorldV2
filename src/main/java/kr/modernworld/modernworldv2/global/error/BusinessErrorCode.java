ddd22f8c837e60b729e32dc9d8c6ff042767dc8d
JYE <jo1814@naver.com>
2025-12-22 22:39:28 +0900

    ✨feat: BusinessErrorCode 유저 관련 예외 추가 (#40)

--- src/main/java/kr/modernworld/modernworldv2/global/error/BusinessErrorCode.java
+++ src/main/java/kr/modernworld/modernworldv2/global/error/BusinessErrorCode.java
@@ -50,6 +50,7 @@ public enum BusinessErrorCode {
 
   USER_NOT_HAS_ENOUGH_POINT(HttpStatus.FORBIDDEN, "User does not have enough point."),
   USER_NOT_FOUND(HttpStatus.FORBIDDEN, "User not found."),
+  USER_NOT_HAS_ENOUGH_CHANCE(HttpStatus.FORBIDDEN, "You do not have enough chance."),
 
   PRESENT_CANNOT_PRESENT_TO_YOURSELF(HttpStatus.FORBIDDEN, "You cannot send a post to yourself."),
   PRESENT_INVALID_STATE(HttpStatus.FORBIDDEN, "Present is not a valid state."),
