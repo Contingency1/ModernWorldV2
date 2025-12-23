935e8a0b016af7d2cc6c615e78f522a29e4ad9c3
JYE <jo1814@naver.com>
2025-12-23 18:43:23 +0900

    ❌delete: RSPMapper 관련 updateEntityFromDomain 메서드 삭제(#40)

--- src/main/java/kr/modernworld/modernworldv2/user/infrastructure/mapper/RSPMapper.java
+++ src/main/java/kr/modernworld/modernworldv2/user/infrastructure/mapper/RSPMapper.java
@@ -4,7 +4,6 @@ import kr.modernworld.modernworldv2.user.domain.rsp.RSP;
 import kr.modernworld.modernworldv2.user.infrastructure.persistence.entity.RspGameRecordJPAEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface RSPMapper {
@@ -14,7 +13,4 @@ public interface RSPMapper {
 
   @Mapping(target = "userNo", source = "user.no")
   RSP toDomain(RspGameRecordJPAEntity rsp);
-
-  @Mapping(target = "user.no", source = "userNo")
-  void updateEntityFromDomain(RSP rsp, @MappingTarget RspGameRecordJPAEntity entity);
 }
