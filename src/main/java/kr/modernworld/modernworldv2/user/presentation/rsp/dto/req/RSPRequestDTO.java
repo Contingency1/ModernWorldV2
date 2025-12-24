707f643e5170293d89dacc75e8677ae01a099137
JYE <jo1814@naver.com>
2025-12-24 14:34:01 +0900

    ❌delete: RSPRequestDTO 기본값 채워주는 생성자 제거 (#40)

--- src/main/java/kr/modernworld/modernworldv2/user/presentation/rsp/dto/req/RSPRequestDTO.java
+++ src/main/java/kr/modernworld/modernworldv2/user/presentation/rsp/dto/req/RSPRequestDTO.java
@@ -3,12 +3,6 @@ package kr.modernworld.modernworldv2.user.presentation.rsp.dto.req;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 
-public record RSPRequestDTO(
-    @Max(2) @Min(0) Integer choice) {
+public record RSPRequestDTO(@Max(2) @Min(0) Integer choice) {
 
-  public RSPRequestDTO {
-    if (choice == null) {
-      choice = 3;
-    }
-  }
 }
