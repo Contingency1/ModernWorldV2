26a04c4f045a70428a210366b4d52c6a3b94bd30
JYE <jo1814@naver.com>
2025-12-28 21:19:57 +0900

    ✨feat: Item, Character 관련 조회 Service 로직 추가 (#45)

--- src/main/java/kr/modernworld/modernworldv2/user/application/shop/CharacterShopService.java
+++ src/main/java/kr/modernworld/modernworldv2/user/application/shop/CharacterShopService.java
@@ -1,9 +1,14 @@
 package kr.modernworld.modernworldv2.user.application.shop;
 
+import java.util.List;
+import kr.modernworld.modernworldv2.admin.application.character.api.CharacterApiDTO;
+import kr.modernworld.modernworldv2.admin.domain.character.CharacterSpecies;
 import kr.modernworld.modernworldv2.admin.domain.character.port.CharacterApi;
 import kr.modernworld.modernworldv2.user.application.characterlocker.CharacterLockerService;
 import kr.modernworld.modernworldv2.user.application.user.UserService;
 import kr.modernworld.modernworldv2.user.domain.characterlocker.CharacterLocker;
+import kr.modernworld.modernworldv2.user.presentation.shop.character.dto.req.ShopCharacterRequestDTO;
+import kr.modernworld.modernworldv2.user.presentation.shop.character.dto.res.ShopCharacterResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,5 +30,20 @@ public class CharacterShopService {
     userService.decreaseCurrentPoint(userNo, characterPrice);
 
     return characterLockerService.addCharacter(userNo, characterNo);
+  }
+
+  @Transactional(readOnly = true)
+  public ShopCharacterResponseDTO getCharacter(Long characterNo) {
+    CharacterApiDTO response = characterApi.getOne(characterNo);
+    return ShopCharacterResponseDTO.from(response);
+  }
+
+  @Transactional(readOnly = true)
+  public List<ShopCharacterResponseDTO> getCharacters(ShopCharacterRequestDTO query) {
+    CharacterSpecies species = CharacterSpecies.stringToCharacterSpecies(query.species());
+    String characterName = query.characterName();
+
+    List<CharacterApiDTO> response = characterApi.getAll(species, characterName);
+    return response.stream().map(ShopCharacterResponseDTO::from).toList();
   }
 }
--- src/main/java/kr/modernworld/modernworldv2/user/application/shop/ItemShopService.java
+++ src/main/java/kr/modernworld/modernworldv2/user/application/shop/ItemShopService.java
@@ -1,10 +1,14 @@
 package kr.modernworld.modernworldv2.user.application.shop;
 
-import kr.modernworld.modernworldv2.admin.domain.item.port.ItemApi;
+import java.util.List;
+import kr.modernworld.modernworldv2.admin.application.item.api.ItemApiDTO;
 import kr.modernworld.modernworldv2.admin.application.item.api.ItemNameAndPriceDTO;
+import kr.modernworld.modernworldv2.admin.domain.item.port.ItemApi;
 import kr.modernworld.modernworldv2.user.application.inventory.InventoryService;
 import kr.modernworld.modernworldv2.user.application.user.UserService;
 import kr.modernworld.modernworldv2.user.domain.inventory.Inventory;
+import kr.modernworld.modernworldv2.user.presentation.shop.item.dto.req.ItemRequestDTO;
+import kr.modernworld.modernworldv2.user.presentation.shop.item.dto.res.ShopItemResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,5 +30,21 @@ public class ItemShopService {
     userService.decreaseCurrentPoint(userNo, itemInfo.price());
 
     return inventoryService.addOneItemInInventory(userNo, itemNo);
+  }
+
+  @Transactional(readOnly = true)
+  public ShopItemResponseDTO getOne(Long itemNo) {
+    ItemApiDTO response = itemApi.getOne(itemNo);
+
+    return ShopItemResponseDTO.from(response);
+  }
+
+  @Transactional(readOnly = true)
+  public List<ShopItemResponseDTO> getAll(ItemRequestDTO query) {
+    return itemApi
+        .getAll(query.theme(), query.itemName())
+        .stream()
+        .map(ShopItemResponseDTO::from)
+        .toList();
   }
 }
