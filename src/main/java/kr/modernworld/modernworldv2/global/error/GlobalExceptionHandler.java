6f68323dc447c998739cfef4b23186c416bcf5c1
JYE <jo1814@naver.com>
2025-12-29 19:11:00 +0900

    ✨feat: JwtValidationCustomException 예외처리 추가 (#18)
    - Advcie를 통해 해당 예외를 처리할수 있도록 설정
    - 기존에는 해당 예외가 Filter에서만 발생 및 처리가 되어서 넣진 않았음.
    - 이제는 Spring 내부에서 발생하는 예외가 있기 때문에, 해당 메서드를 추가함.

--- src/main/java/kr/modernworld/modernworldv2/global/error/GlobalExceptionHandler.java
+++ src/main/java/kr/modernworld/modernworldv2/global/error/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package kr.modernworld.modernworldv2.global.error;
 
 import jakarta.validation.ConstraintViolationException;
 import java.util.stream.Collectors;
+import kr.modernworld.modernworldv2.user.infrastructure.auth.jwt.JwtValidationCustomException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +28,17 @@ public class GlobalExceptionHandler {
     );
 
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+  }
+
+  @ExceptionHandler(JwtValidationCustomException.class)
+  public ResponseEntity<ErrorResponseDTO> handleAllException(JwtValidationCustomException ex) {
+    ErrorResponseDTO response = new ErrorResponseDTO(
+        BusinessErrorCode.INVALID_REFRESH_TOKEN.getMessage() + " " + ex.getMessage(),
+        BusinessErrorCode.INVALID_REFRESH_TOKEN.getStatus().getReasonPhrase(),
+        BusinessErrorCode.INVALID_REFRESH_TOKEN.getStatus().value()
+    );
+
+    return new ResponseEntity<>(response, BusinessErrorCode.INVALID_REFRESH_TOKEN.getStatus());
   }
 
   // Validation Error.
@@ -93,7 +105,7 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(BusinessException.class)
   public ResponseEntity<ErrorResponseDTO> handleBusinessException(BusinessException ex) {
     ErrorResponseDTO response = new ErrorResponseDTO(ex.getMessage(),
-        ex.getErrorCode().getMessage(), ex.getErrorCode().getStatus().value());
+        ex.getErrorCode().getStatus().getReasonPhrase(), ex.getErrorCode().getStatus().value());
 
     return new ResponseEntity<>(response, ex.getErrorCode().getStatus());
   }
