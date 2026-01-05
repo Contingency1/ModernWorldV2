5b8b07e5e7fc79861b91000c0c740190e299afea
JYE <jo1814@naver.com>
2026-01-05 19:53:04 +0900

    🛠️fix: PresentCreatedEvent 관련 놓쳤던 인자 추가 (#51)

--- src/main/java/kr/modernworld/modernworldv2/asset/domain/present/event/PresentCreatedEvent.java
+++ src/main/java/kr/modernworld/modernworldv2/asset/domain/present/event/PresentCreatedEvent.java
@@ -1,6 +1,7 @@
 package kr.modernworld.modernworldv2.asset.domain.present.event;
 
 public record PresentCreatedEvent(
+    Long senderNo,
     Long receiverNo,
     String senderName,
     String itemName
