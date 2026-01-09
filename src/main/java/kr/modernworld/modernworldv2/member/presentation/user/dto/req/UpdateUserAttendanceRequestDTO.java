712073084cfdd0c5aed2db81847b1550feec0659
JYE <jo1814@naver.com>
2026-01-09 18:40:13 +0900

    🛠️fix: RequestDTO 관련 null Validation 설정 (#51)

--- src/main/java/kr/modernworld/modernworldv2/member/presentation/user/dto/req/UpdateUserAttendanceRequestDTO.java
+++ src/main/java/kr/modernworld/modernworldv2/member/presentation/user/dto/req/UpdateUserAttendanceRequestDTO.java
@@ -2,10 +2,12 @@ package kr.modernworld.modernworldv2.member.presentation.user.dto.req;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 public record UpdateUserAttendanceRequestDTO(
     @Min(1)
     @Max(10)
+    @NotNull
     Integer stickerNo
 ) {
 
