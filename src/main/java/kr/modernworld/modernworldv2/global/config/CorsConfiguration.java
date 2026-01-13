cfb0c2acda2e29ecda52ffc981b429fb65ad4cd2
JYE <jo1814@naver.com>
2026-01-13 15:25:37 +0900

    ✨feat: CORS 관련 설정 (#53)

--- src/main/java/kr/modernworld/modernworldv2/global/config/CorsConfiguration.java
+++ src/main/java/kr/modernworld/modernworldv2/global/config/CorsConfiguration.java
@@ -1,15 +1,33 @@
 package kr.modernworld.modernworldv2.global.config;
 
-import org.apache.catalina.filters.CorsFilter;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfiguration {
 
+  @Value("${cors.allowed-origins}")
+  private List<String> allowedOrigins;
+
   @Bean
   public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
-    return new CorsFilter();
+    org.springframework.web.cors.CorsConfiguration config
+        = new org.springframework.web.cors.CorsConfiguration();
+
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(allowedOrigins);
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+
+    source.registerCorsConfiguration("/**", config);
+
+    return new CorsFilter(source);
   }
 }
